b20f0b6f711594fe6a7b1a0bc78b3d791661a403
Magnus Norden <magnus.norden@it.su.se>
2016-11-03 07:20:28 +0100

    some basics

--- grails-app/controllers/se/su/it/flex/DashboardController.groovy
+++ grails-app/controllers/se/su/it/flex/DashboardController.groovy
@@ -6,8 +6,25 @@ class DashboardController {
   def index() {
     Employee employee = (session.getAttribute('uid')) ? Employee.findByUid(session.getAttribute('uid') as String) : null
     int dailyDelta = (session.getAttribute('uid')) ? flexService.getAggregatedDeltaForUser(session.getAttribute('uid') as String) : 0
-    List<Absence> absences = (employee) ? Absence.findAllByEmployee(employee, [sort: 'calendar.workDate', order: 'desc']) : []
     List<ReportedTime> reportedTimes = (employee) ? ReportedTime.findAllByEmployee(employee, [max: 25, sort: 'calendar.workDate', order: 'desc']) : []
-    [absences: absences, dailyDelta: dailyDelta, employee: employee, reportedTimes: reportedTimes]
+    [dailyDelta: dailyDelta, employee: employee, reportedTimes: reportedTimes]
+  }
+
+  def show() {
+    if(!params.long('id')) {
+      log.warn "Missing id for ReportedTime"
+      flash.message = "Missing id for ReportedTime"
+      return redirect(action: 'index')
+    }
+    ReportedTime reportedTime = ReportedTime.get(params.long('id'))
+    if(!reportedTime) {
+      log.warn "Cant find ReportedTime for ${params.long('id')}"
+      flash.message = "Cant find ReportedTime for ${params.long('id')}"
+      return redirect(action: 'index')
+    }
+    List<Absence> absences = Absence.findAllByEmployeeAndCalendar(reportedTime.employee, reportedTime.calendar)
+    List<TimeAdjustment> timeAdjustments = TimeAdjustment.findAllByEmployeeAndCalendar(reportedTime.employee, reportedTime.calendar)
+    WorkRate workRate = WorkRate.findByEmployeeAndStartDateLessThanEqualsAndEndDateGreaterThanEquals(reportedTime.employee, reportedTime.calendar.workDate, reportedTime.calendar.workDate, [max: 1])
+    [absences: absences, reportedTime: reportedTime, timeAdjustments: timeAdjustments, workRate: workRate]
   }
 }
---
+++ grails-app/views/sysAdmin/show.gsp
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta name="layout" content="flex"/>
+    <title>Flex Dashboard : ${employee?.uid}</title>
+    <asset:javascript src="dashboard/index.js"/>
+  </head>
+  <body>
+    <div class="container">
+      hoho
+    </div>
+  </body>
+</html>
